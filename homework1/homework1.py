0564aa3fc82bd3349e1fe4a44d5b3c2d1d39c428
Björn Skoglund <bjorn.skoglund@gmail.com>
2014-07-03 22:44:11 +0200

    Solved second question.

--- homework1/homework1.py
+++ homework1/homework1.py
@@ -1,16 +1,23 @@
 import sys
 import heapq
 
+
+def difference((weight, length)):
+    return (-weight + length, -weight, -length)
+
+def quota((weight, length)):
+    return (-float(weight) / length, -weight, -length)
+
 def get_entry(f):
     line = f.readline().strip()
     left, right = line.split(' ')
     weight, length = (int(left), int(right))
-    return (-weight + length, -weight, -length)
+    return weight, length
 
-def weighted_sum(filename):
+def weighted_sum(filename, objective):
     f = open(filename, 'r')
     num_lines = int(f.readline())
-    x = [get_entry(f) for _ in range(num_lines)]
+    x = [objective(get_entry(f)) for _ in range(num_lines)]
 
     heapq.heapify(x)
     t = 0
@@ -31,8 +38,11 @@ if __name__ == '__main__':
         print "usage: ./homework1.py [1|2|3] <inputfile>"
         sys.exit(0)
     question = sys.argv[1]
+    filename = sys.argv[2]
     if question == '1':
-        filename = sys.argv[2]
-        weighted_sum(filename)
+        
+        weighted_sum(filename, difference)
+    elif question == '2':
+        weighted_sum(filename, quota)
     else:
         print 'Unknown assignment: ' + question
